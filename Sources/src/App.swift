7b154ab31c19b9d81e7f6269577fb77f789b7cc1
Wilhelm Oks <>
2023-05-21 15:53:46 +0200

    prototype title text

--- Sources/src/App.swift
+++ Sources/src/App.swift
@@ -13,6 +13,8 @@ struct TokamakApp: App {
 struct ContentView: View {
     var body: some View {
         VStack {
+            Text("This is a basic Web Assembly prototype app built with Swift, using Tokamak for UI, which uses a SwiftUI like API.")
+            
             PrototypeView()
         }
         .padding()
